3d79340156241ee339dfad5d1cc5497b33f7d130
RooRsmoka <chri.disa96@gmail.com>
2022-06-03 16:54:27 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_static.Models;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public static ListaPizze pizze;
+        public IActionResult Index()
+        {
+            pizze = new ListaPizze();
+                
+            Pizza pizzaOne = new Pizza("Pizza Marinara", "Ingredienti: Pomodoro, Origano, Aglio, Basilico", "img/Marinara.jpg", 4.50);
+            Pizza pizzaTwo = new Pizza("Pizza Margherita", "Ingredienti: Pomodoro, Mozzarella,Basilico", "img/Margherita.jpg", 5.00);
+            Pizza pizzaThree = new Pizza("Pizza Cotto&Funghi", "Ingredienti: Pomodoro, Mozzarella, Prosc. Cotto, Funghi Porcini", "img/Cotto&Funghi.jpg", 6.00);
+            Pizza pizzaFour = new Pizza("Pizza Quattro Formaggi", "Ingredienti: Pomodoro, Mozzarella, Fontina, Gorgonzola, Parmigiano Reggiano", "img/QuattroFormaggi.jpg", 6.50);
+            Pizza pizzaFive = new Pizza("Pizza Salame", "Ingredienti: Pomodoro, Mozzarella, Salame Milano", "img/Salame.jpg", 5.50);
+
+
+            return View();
+        }
+
+        public IActionResult ShowPizze()
+        {
+            return View("ShowPizze");
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,29 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Foto { get; set; }
+        public double Prezzo { get; set; }
+
+        public Pizza(string Nome, string Descrizione, string Foto, double Prezzo)
+        {
+            this.Nome = Nome;
+            this.Descrizione = Descrizione;
+            this.Foto = Foto;
+            this.Prezzo = Prezzo;
+        }
+
+        public class ListaPizze
+        {
+            public List<Pizza> lsPizze { get; set; }
+
+            public ListaPizze()
+            {
+                lsPizze = new();
+            }
+        }
+    }
+}
